cd3522d7d13d1c100f0db5c91665bfcd83a31495
koga yushi <youshe.old.pleasure@gmail.com>
2020-07-04 23:35:34 +0900

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-cloudfront-s3-circleci-sample
-      </h1>
-      <h2 class="subtitle">
-        My brilliant Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="title">
+      sample app on {{ environment }}
+    </h1>
   </div>
 </template>
 
@@ -34,6 +12,11 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  computed: {
+    environment() {
+      return process.env.ENVIRONMENT
+    }
   }
 }
 </script>
@@ -57,16 +40,5 @@ export default {
   color: #35495e;
   letter-spacing: 1px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
